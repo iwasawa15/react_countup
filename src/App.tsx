5f9c1494fba77efd419b095f6f2078cff28bfd84
iwasawa15 <tatsuya57sak@gmail.com>
2019-10-04 17:16:25 +0900

    カウント部分の定義

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,16 @@ import './App.css';
 const App: React.FC = () => {
   const [counter, CountUp] = useState(0);
   
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          count: {counter}
+        </p>
+        <button onClick={() => CountUp(counter + 1)}>push</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
